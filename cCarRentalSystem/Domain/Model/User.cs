7e6d8bdde883bb009b9309f3a12e11938a50b0cc
apurveh <apurvamishra573@gmail.com>
2023-10-31 10:14:42 +0100

    User Class added

---
+++ cCarRentalSystem/Domain/Model/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Model;
+
+public class User
+{
+    private string FirstName { get; }
+    private string LastName { get; }
+    private string Email { get; }
+    private string DateOfBirth { get; }
+    private int CPRNumber { get; }
+    private int PhoneNumber { get; }
+
+
+    public User(string firstName, string lastName, string email,string dateOfBirth,int cprNumber,int phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        DateOfBirth = dateOfBirth;
+        CPRNumber = cprNumber;
+        PhoneNumber = phoneNumber;
+    }
+
+}
