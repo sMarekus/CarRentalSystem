ef2cfe101187caef4e9ea315cf43d14a3f9a15d8
Marekus <marek.stransky1@gmail.com>
2023-11-02 15:50:06 +0100

    EFC part

---
+++ CarRentalData/EfcDataAccess/CarRentalSystemContext.cs
@@ -0,0 +1,15 @@
+﻿using Entity.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfcDataAccess;
+
+public class CarRentalSystemContext : DbContext
+{
+    public DbSet<UserEntity?> Users { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = Todo.db");
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);            
+    }
+}
