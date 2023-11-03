1bd1d3ba6ac6014794a43f56b56dcc30d707382e
archdegenerate <tomi.masiar@gmail.com>
2023-11-03 13:10:14 +0100

    Register implementation

--- cCarRentalSystem/Client/BlazorApp/Program.cs
+++ cCarRentalSystem/Client/BlazorApp/Program.cs
@@ -8,12 +8,22 @@ using Domain.Auth;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using System.Security.Claims;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(
+    sp => 
+        new HttpClient { 
+            BaseAddress = new Uri("https://localhost:8080") 
+        }
+);
+builder.Services.AddScoped<ICarService, CarService>();
+builder.Services.AddScoped<IUserService, UserService>();
 AuthorizationPolicies.AddPolicies(builder.Services);
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<IAuthService, JwtAuthService>();
