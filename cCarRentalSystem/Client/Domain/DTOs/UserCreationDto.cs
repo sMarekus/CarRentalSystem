ba995a920b26e6c4df36f7d7779dd6f6cfb9e0bc
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-11-07 14:58:08 +0100

    User edit + Blazor

--- cCarRentalSystem/Client/Domain/DTOs/UserCreationDto.cs
+++ cCarRentalSystem/Client/Domain/DTOs/UserCreationDto.cs
@@ -4,19 +4,23 @@ namespace Domain.DTOs;
 
 public class UserCreationDto
 {
-    public long Cpr { get; }
-    public string Name { get; }
-    public string Surname { get; }
-    public string Password { get; }
-    public bool IsAdmin { get; }
+    public string FirstName { get;  }
+    public string LastName { get;  }
+    public string Email { get;  }
+    public string Password { get;  }
+    public long CprNumber { get;  }
+    public long PhoneNumber { get;  }
+    public bool IsAdmin { get;  }
 
 
-    public UserCreationDto(long cpr, string name, string surname, string password, bool isAdmin)
+    public UserCreationDto(string firstName, string lastName, string email, string password, long cprNumber, long phoneNumber, bool isAdmin)
     {
-        Cpr = cpr;
-        Name = name;
-        Surname = surname;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
         Password = password;
+        CprNumber = cprNumber;
+        PhoneNumber = phoneNumber;
         IsAdmin = isAdmin;
     }
 }
