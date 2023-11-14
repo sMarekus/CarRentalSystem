c8cccce22ca27568280ba1a261242dab8f31edb8
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-11-14 14:41:06 +0100

    jwt

--- CarRentalService/src/main/java/com/example/carrentalservice/Jwt/JwtService.java
+++ CarRentalService/src/main/java/com/example/carrentalservice/Jwt/JwtService.java
@@ -1,24 +1,21 @@
 package com.example.carrentalservice.Jwt;
-import com.example.carrentalservice.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jdk.jshell.spi.ExecutionControl;
-import org.apache.el.lang.FunctionMapperImpl;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
 
     private static final String SECRET_KEY= "oursecretkeywhichhasmorethan16characters";
 
-    public String extractUsername(String token){
+    public String extractCpr(String token){
         return extractClaim(token, Claims::getSubject);
     }
 
@@ -47,10 +44,10 @@ public class JwtService {
     }
 
 
-    /*public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String cpr = extractCpr(token);
+        return cpr.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
-     */
+
 }
---
+++ CarRentalService/src/main/java/com/example/carrentalservice/Jwt/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.example.carrentalservice.Jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    /**
+     * Configures the security filter chain for handling HTTP security configurations.
+     *
+     * @param http The HttpSecurity object to configure.
+     * @return The configured SecurityFilterChain.
+     * @throws Exception If an exception occurs during configuration.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests( authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/users")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/users/authenticate")).permitAll()
+                        .anyRequest().authenticated());
+
+        http.authenticationProvider(authenticationProvider);
+
+        http.addFilterBefore((Filter) jwtAuthFilter, (Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
