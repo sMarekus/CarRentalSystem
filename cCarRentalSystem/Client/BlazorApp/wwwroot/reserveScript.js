2c63364905808ff58eb6d792d9920445d3705220
archdegenerate <tomi.masiar@gmail.com>
2023-12-04 12:14:12 +0100

    reservation prep

---
+++ cCarRentalSystem/Client/BlazorApp/wwwroot/reserveScript.js
@@ -0,0 +1,13 @@
+﻿document.getElementById('openBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === document.getElementById('popup')) {
+        document.getElementById('popup').style.display = 'none';
+    }
+});
