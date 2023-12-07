5142670f8633297438dc4c0ad82fd0535fe25b42
Marekus <marek.stransky1@gmail.com>
2023-12-07 13:20:31 +0100

    Create reservation

--- cCarRentalSystem/Client/Domain/Models/Reservation.cs
+++ cCarRentalSystem/Client/Domain/Models/Reservation.cs
@@ -3,15 +3,15 @@ namespace Domain.Models;
 public class Reservation
 {
     public int Id { get; set; }
-    public string Username{get;set;}
+    public string UserName{get;set;}
     public int CarId{get;set;}
     public DateTime StartDate{get;set;}
     public DateTime EndDate{get;set;}
     public double TotalPrice{get;set;}
-    public Reservation(string username, int carId, DateTime startDate, DateTime endDate, double totalPrice)
+    public Reservation(int carId, string username, DateTime startDate, DateTime endDate, double totalPrice)
     {
-        Username = username;
         CarId = carId;
+        UserName = username;
         StartDate = startDate;
         EndDate = endDate;
         TotalPrice = totalPrice;
